ff9be81703b0b6dbeacb03f0785acedd2e19d449
Eoin Hanan <14176459@studentmail.ul.ie>
2018-03-11 13:29:41 +0000

    Sync is on its way to working.

--- app/src/main/java/com/example/eoinh/fallscatcherv3/Api.java
+++ app/src/main/java/com/example/eoinh/fallscatcherv3/Api.java
@@ -5,9 +5,15 @@ package com.example.eoinh.fallscatcherv3;
  */
 
 public class Api {
-    private static final String ROOT_URL = "http://10.52.54.189/FallsApi/v1/Api.php?apicall=createfall";    public static final String URL_CREATE_FALL = ROOT_URL + "createfall";
-    public static final String URL_READ_FALLS = ROOT_URL + "getfalls";
-    public static final String URL_UPDATE_FALL = ROOT_URL + "updatefalls";
-    public static final String URL_DELETE_FALL = ROOT_URL + "deletefalls&id=";
+//    private static final String ROOT_URL = "http://10.52.54.189/FallsApi/v1/Api.php?apicall=createfall";
+//    public static final String URL_CREATE_FALL = ROOT_URL + "createfall";
+//    public static final String URL_READ_FALLS = ROOT_URL + "getfalls";
+//    public static final String URL_UPDATE_FALL = ROOT_URL + "updatefalls";
+//    public static final String URL_DELETE_FALL = ROOT_URL + "deletefalls&id=";
+    private static final String ROOT_URL = "http://10.52.2.122/sync2/v1/Api.php?apicall=";
 
+    public static final String URL_CREATE_HERO = ROOT_URL + "createFall";
+    public static final String URL_READ_HEROES = ROOT_URL + "getheroes";
+    public static final String URL_UPDATE_HERO = ROOT_URL + "updatehero";
+    public static final String URL_DELETE_HERO = ROOT_URL + "deletehero&id=";
 }
--- app/src/main/java/com/example/eoinh/fallscatcherv3/SettingsFragment.java
+++ app/src/main/java/com/example/eoinh/fallscatcherv3/SettingsFragment.java
@@ -1,24 +1,31 @@
 package com.example.eoinh.fallscatcherv3;
 
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AlertDialog;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.util.*;
+
+import static android.view.View.GONE;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -54,8 +61,6 @@ public class SettingsFragment extends Fragment {
         notification = new NotificationCompat.Builder(getActivity(), "Channel id");
         notification.setAutoCancel(true);
 
-
-
         reminderButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
@@ -81,7 +86,176 @@ public class SettingsFragment extends Fragment {
     public void setNotification(boolean isOn){
 
     }
+    public void syncFalls() {
+        createHero();
+    }
 
+    private void createHero() {
+        //Calling the create hero API
+        HashMap<String, String> params = new HashMap<>();
+        params.put("localFallID", "2");
+        params.put("patientID", "1");
+        params.put("date", "1898-03-03");
+        params.put("timeStatus", "Estimate");
+
+        PerformNetworkRequest request = new PerformNetworkRequest(Api.URL_CREATE_HERO, params, CODE_POST_REQUEST);
+        request.execute();
+    }
+
+    private class PerformNetworkRequest extends AsyncTask<Void, Void, String> {
+
+        String url;
+
+        HashMap<String, String> params;
+
+        int requestCode;
+
+        PerformNetworkRequest(String url, HashMap<String, String> params, int requestCode) {
+            this.url = url;
+            this.params = params;
+            this.requestCode = requestCode;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                JSONObject object = new JSONObject(s);
+                if (!object.getBoolean("error")) {
+                    Toast.makeText(getActivity().getApplicationContext(), object.getString("message"), Toast.LENGTH_SHORT).show();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            RequestHandler requestHandler = new RequestHandler();
+
+            if (requestCode == CODE_POST_REQUEST)
+                return requestHandler.sendPostRequest(url, params);
+
+
+            if (requestCode == CODE_GET_REQUEST)
+                return requestHandler.sendGetRequest(url);
+
+            return null;
+        }
+    }
+
+//    class HeroAdapter extends ArrayAdapter<Hero> {
+//
+//        //our hero list
+//        List<Hero> heroList;
+//
+//
+//        //constructor to get the list
+//        public HeroAdapter(List<Hero> heroList) {
+//            super(MainActivity.this, R.layout.layout_hero_list, heroList);
+//            this.heroList = heroList;
+//        }
+//
+//
+//        //method returning list item
+//        public View getView(int position, View convertView, ViewGroup parent) {
+//            LayoutInflater inflater = getLayoutInflater();
+//            View listViewItem = inflater.inflate(R.layout.layout_hero_list, null, true);
+//
+//            //getting the textview for displaying name
+//            TextView textViewName = listViewItem.findViewById(R.id.textViewName);
+//
+//            //the update and delete textview
+//            TextView textViewUpdate = listViewItem.findViewById(R.id.textViewUpdate);
+//            TextView textViewDelete = listViewItem.findViewById(R.id.textViewDelete);
+//
+//            final Hero hero = heroList.get(position);
+//
+//            textViewName.setText(hero.getName());
+//
+//            //attaching click listener to update
+//            textViewUpdate.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    //so when it is updating we will
+//                    //make the isUpdating as true
+//                    isUpdating = true;
+//
+//                    //we will set the selected hero to the UI elements
+//                    editTextHeroId.setText(String.valueOf(hero.getId()));
+//                    editTextName.setText(hero.getName());
+//                    editTextRealname.setText(hero.getRealname());
+//                    ratingBar.setRating(hero.getRating());
+//                    spinnerTeam.setSelection(((ArrayAdapter<String>) spinnerTeam.getAdapter()).getPosition(hero.getTeamaffiliation()));
+//
+//                    //we will also make the button text to Update
+//                    buttonAddUpdate.setText("Update");
+//                }
+//            });
+//
+//            //when the user selected delete
+//            textViewDelete.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//
+//                    // we will display a confirmation dialog before deleting
+//                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+//
+//                    builder.setTitle("Delete " + hero.getName())
+//                            .setMessage("Are you sure you want to delete it?")
+//                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+//                                public void onClick(DialogInterface dialog, int which) {
+//                                    //if the choice is yes we will delete the hero
+//                                    //method is commented because it is not yet created
+//                                    //deleteHero(hero.getId());
+//                                }
+//                            })
+//                            .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+//                                public void onClick(DialogInterface dialog, int which) {
+//                                }
+//                            })
+//                            .setIcon(android.R.drawable.ic_dialog_alert)
+//                            .show();
+//
+//                }
+//            });
+//
+//            return listViewItem;
+//        }
+//    }
+
+    private void readHeroes() {
+        PerformNetworkRequest request = new PerformNetworkRequest(Api.URL_READ_HEROES, null, CODE_GET_REQUEST);
+        request.execute();
+    }
+
+//    private void updateHero() {
+//
+//
+//
+//        PerformNetworkRequest request = new PerformNetworkRequest(Api.URL_UPDATE_HERO, params, CODE_POST_REQUEST);
+//        request.execute();
+//
+//        buttonAddUpdate.setText("Add");
+//
+//        editTextName.setText("");
+//        editTextRealname.setText("");
+//        ratingBar.setRating(0);
+//        spinnerTeam.setSelection(0);
+//
+//        isUpdating = false;
+//    }
+//
+//    private void deleteHero(int id) {
+//        PerformNetworkRequest request = new PerformNetworkRequest(Api.URL_DELETE_HERO + id, null, CODE_GET_REQUEST);
+//        request.execute();
+//    }
+/*
     public void syncFalls() {
 
         ArrayList <Fall> falls = db.getFalls();
@@ -193,6 +367,6 @@ public class SettingsFragment extends Fragment {
             return null;
         }
     }
-
+*/
 
 }
